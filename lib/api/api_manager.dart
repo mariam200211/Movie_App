f15d46293d4107dac16dbde3fe887c4ccf723f61
abc <abc@example.com>
2024-08-30 22:06:47 +0300

    Api category

--- lib/api/api_manager.dart
+++ lib/api/api_manager.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:movies_app/api/api_constatns.dart';
+import 'package:movies_app/model/category.dart';
 import 'dart:developer';
 import 'package:movies_app/model/movie_model.dart';
 import 'package:movies_app/model/movie_recommendation_mode.dart';
@@ -53,18 +54,21 @@ class ApiServices {
  }
 
 
-
- Future<MovieRecommendationsModel> getMovieRecommendations(int movieId) async {
-  endPoint = 'movie/$movieId/recommendations';
+ Future<category> getCategory() async {
+  endPoint = 'genre/movie/list';
   final url = '$baseUrl$endPoint$key';
 
   final response = await http.get(Uri.parse(url));
   if (response.statusCode == 200) {
    log('success');
-   return MovieRecommendationsModel.fromJson(jsonDecode(response.body));
+
+   return category.fromJson(jsonDecode(response.body));
   }
-  throw Exception('failed to load  movie details');
+  throw Exception('failed to load top rated series');
  }
+
+
+
 
 
 }
